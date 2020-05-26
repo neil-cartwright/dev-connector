293470af2d76f6b19c032ef0f48e4fb5d25de3f7
neil-cartwright <neil@sitepages.co.uk>
2020-05-26 09:18:32 +0100

    up to registering users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,84 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const {
+    check,
+    validationResult
+} = require('express-validator');
 
 /**
  * 
  * @route   GET api / Users
- * @desc    Test Route
+ * @desc    Register user
  * @access  Public
  * 
  * */
+const User = require('../../models/User');
 
-router.get('/', (req, res) => res.send('User route'));
+router.post('/',
+    [
+        check('name', 'Name required please').not().isEmpty(),
+        check('email', 'Email required please').isEmail(),
+        check('password', 'Please enter a password with six or more characters').isLength({
+            min: 6
+        })
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            })
+        }
+
+        const {
+            name,
+            email,
+            password
+        } = req.body
+
+        try {
+            // see if user exists
+            let user = await User.findOne({
+                email
+            });
+            if (user) {
+                return res.status(400).json({
+                    errors: [{
+                        msg: 'User already exits'
+                    }]
+                });
+            }
+            // get users gravatar
+            const avatar = gravatar.url(email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            })
+
+            user = new User({
+                name,
+                email,
+                avatar,
+                password
+            })
+            // encrypt password
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+
+            // return jasonwebtoken
+
+            res.send('User registered');
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).snd('server error')
+        }
+
+
+
+    });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();
 // connect database
 connectDB();
 
+// init middleware
+app.use(express.json({
+    extended: false
+}));
+
 app.get('/', (req, res) => res.send('API running'));
 
 // Define routes
