294a22ef133f98989d30abf4f5b9a25a33c73d57
neil-cartwright <neil@sitepages.co.uk>
2020-05-29 11:00:08 +0100

    added redux. and alert component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, {Fragment, useState} from "react";
+import {Link} from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onChange = (e) =>
+    setFormData({...formData, [e.target.name]: e.target.value});
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Success");
+  };
+
+  const {email, password} = formData;
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign in to Tour Account
+      </p>
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => onChange(e)}
+          placeholder="Email Address"
+          name="email"
+          required
+        />
+        <input
+          value={password}
+          onChange={(e) => onChange(e)}
+          type="password"
+          placeholder="Password"
+          name="password"
+          minLength="6"
+        />
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign up</Link>
+      </p>
+    </Fragment>
+  );
 };
 
 export default Login;
