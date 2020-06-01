9adb24ae808e74ec7acdad04ec738d037099fed0
neil-cartwright <neil@sitepages.co.uk>
2020-06-01 21:59:13 +0100

    added dashboard actions

--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -4,6 +4,7 @@ import {Link} from "react-router-dom";
 import {connect} from "react-redux";
 import {getCurrentProfile} from "../../actions/profile";
 import Spinner from "../layout/spinner";
+import DashboardActions from "./DashboardActions";
 
 const Dashboard = ({
   getCurrentProfile,
@@ -23,7 +24,9 @@ const Dashboard = ({
         <i className="fas fa-user"></i>Welcome {user && user.name}
       </p>
       {profile !== null ? (
-        <Fragment>has</Fragment>
+        <Fragment>
+          <DashboardActions />
+        </Fragment>
       ) : (
         <Fragment>
           <p>You have not yet set up a profile. Please add some info.</p>
