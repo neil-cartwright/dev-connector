05b0a28f311f73b94ab9204f3794a2fe9efa75dc
neil-cartwright <neil@sitepages.co.uk>
2020-06-01 11:19:40 +0100

    moved on a chapter. added spinner and font awesome script

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -80,7 +80,7 @@ export const login = (email, password) => async (dispatch) => {
   } catch (err) {
     const errors = err.response.data.errors;
     if (errors) {
-      errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
+      errors.forEach((error) => dispatch(setAlert((error.msg, "danger"))));
     }
     dispatch({
       type: LOGIN_FAIL,
--- client/src/components/dashboard/dashboard.js
+++ client/src/components/dashboard/dashboard.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, {Fragment, useEffect} from "react";
 import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import {getCurrentProfile} from "../../actions/profile";
+import Spinner from "../layout/spinner";
 
-const dashboard = (props) => {
-  return <div>Dashboard</div>;
+const Dashboard = ({
+  getCurrentProfile,
+  auth: {user},
+  profile: {profile, loading},
+}) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, [getCurrentProfile]);
+
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i>Welcome {user && user.name}
+      </p>
+    </Fragment>
+  );
 };
 
-dashboard.propTypes = {};
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+};
 
-export default dashboard;
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  profile: state.profile,
+});
+
+export default connect(mapStateToProps, {getCurrentProfile})(Dashboard);
