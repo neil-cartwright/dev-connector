5202831e5b5227b54c566c0a449533a4a1fda3db
neil-cartwright <neil@sitepages.co.uk>
2020-06-04 16:52:02 +0100

    adding individual profile pages

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -81,7 +81,9 @@ router.post(
     if (status) profileFields.status = status;
     if (githubusername) profileFields.githubusername = githubusername;
     if (skills) {
-      profileFields.skills = skills.split(",").map((skill) => skill.trim());
+      profileFields.skills = skills
+        .split(",")
+        .map((skill) => " " + skill.trim());
       // re-edited from original version of course. glitchy
     }
     // build social object
