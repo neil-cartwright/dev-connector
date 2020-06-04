303f643a5bd3c2dcdeae6c6a6d45f85a01e3d8c2
neil-cartwright <neil@sitepages.co.uk>
2020-06-04 13:45:06 +0100

    completed chapter 54 display profiles.

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import CreateProfile from "./components/profile-forms/createProfile";
 import EditProfile from "./components/profile-forms/EditProfile";
 import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
-
+import Profiles from "./components/profiles/Profiles";
 // Redux
 import {Provider} from "react-redux";
 import store from "./store";
@@ -39,6 +39,7 @@ const App = () => {
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/profiles" component={Profiles} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute
                 exact
