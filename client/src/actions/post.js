146039b527e5eb13332911422082f6f04df461d3
neil-cartwright <neil@sitepages.co.uk>
2020-06-05 18:44:01 +0100

    delete post functionality added

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import {setAlert} from "./alert";
-import {GET_POSTS, POST_ERROR} from "./types";
+import {GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST} from "./types";
 
 // get posts
 export const getPosts = () => async (dispatch) => {
@@ -10,6 +10,55 @@ export const getPosts = () => async (dispatch) => {
       type: GET_POSTS,
       payload: res.data,
     });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {msg: err.response.statusText, status: err.response.status},
+    });
+  }
+};
+
+// add like
+export const addLike = (id) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/like/${id}`);
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: {id, likes: res.data},
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {msg: err.response.statusText, status: err.response.status},
+    });
+  }
+};
+
+// REMOVE like
+export const removeLike = (id) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/unlike/${id}`);
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: {id, likes: res.data},
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {msg: err.response.statusText, status: err.response.status},
+    });
+  }
+};
+
+// DELETE post
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    const res = await axios.delete(`/api/posts/${id}`);
+    dispatch({
+      type: DELETE_POST,
+      payload: id,
+    });
+    dispatch(setAlert("Post removed", "success"));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
